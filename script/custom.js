6b2c91bd64a45daceff6b0fb92b99082116922db
jinyong1382 <86578173+jinyong1382@users.noreply.github.com>
2021-06-28 15:48:19 +0900

    Update custom.js

--- script/custom.js
+++ script/custom.js
@@ -1,32 +1,29 @@
-$(".nav > ul > li").mouseover(function(){
-//                    $(".nav > ul > li > .subMenu")
+$(".nav > ul > li").mouseover(function () {
     $(this).find(".submenu").stop().slideDown(200);
 });
-$(".nav > ul > li").mouseout(function(){
+$(".nav > ul > li").mouseout(function () {
     $(this).find(".submenu").stop().slideUp(200);
 });
 
 
 
-var currentIndex = 0;
+$(".slidelist").children("div:gt(0)").hide();
+var current = 0;
 
-setInterval(function(){
-    if(currentIndex < 2){
-        currentIndex++;
-    }else{
-        currentIndex = 0;
-    }
-    var slidePosition = currentIndex * (-360)+"px";
-    $(".slidelist").animate({top: slidePosition}, 400);
+setInterval(function () {
+    var next = (current + 1) % 3;
+    $(".slidelist").find("div").eq(current).fadeOut();
+    $(".slidelist").find("div").eq(next).fadeIn();
+    current = next;
 }, 3000);
 
 
 
-$(".line-over1").click(function(e){
+$(".line-over1").click(function (e) {
     e.preventDefault();
     $(".tab-contlayer").show();
 });
-$(".tab-contlayer .close").click(function(e){
+$(".tab-contlayer .close").click(function (e) {
     e.preventDefault();
     $(".tab-contlayer").hide();
 });
